c20a768a4e5b1148d2520757f21bbd1e76ad94fd
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-02-02 09:26:52 +0000

    handling normal vectors

--- src/color.rs
+++ src/color.rs
@@ -4,31 +4,10 @@ pub type Color = V3;
 
 impl Color {
     pub fn print_ppm(&self) {
-        // TODO: really this should be a different type with checks on construction
-        debug_assert!(
-            self.x >= 0.0 && self.x <= 1.0,
-            "self.x out of bounds {}",
-            self.x
-        );
-        debug_assert!(
-            self.y >= 0.0 && self.y <= 1.0,
-            "self.y out of bounds {}",
-            self.y
-        );
-        debug_assert!(
-            self.z >= 0.0 && self.z <= 1.0,
-            "self.z out of bounds {}",
-            self.z
-        );
-
         // Translate the [0,1] component values to the byte range [0,255].
         let ir = (255.999 * self.x) as i64;
         let ig = (255.999 * self.y) as i64;
         let ib = (255.999 * self.z) as i64;
-
-        debug_assert!(ir >= 0 && ir <= 255, "ir out of bounds {ir}");
-        debug_assert!(ig >= 0 && ig <= 255, "ig out of bounds {ig}");
-        debug_assert!(ib >= 0 && ib <= 255, "ib out of bounds {ib}");
 
         println!("{ir} {ig} {ib}");
     }
--- src/ray.rs
+++ src/ray.rs
@@ -17,7 +17,30 @@ impl Ray {
         self.orig + t * self.dir
     }
 
+    /// The derivation of the calculation here is given in section 5 of Ray tracing in one weekend
+    /// https://raytracing.github.io/books/RayTracingInOneWeekend.html
+    fn hits_sphere(&self, center: &Point3, radius: f64) -> f64 {
+        let oc = *center - self.orig;
+
+        let a = self.dir.square_length();
+        let b = -2.0 * self.dir.dot(&oc);
+        let c = oc.square_length() - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            -1.0
+        } else {
+            (-b - discriminant.sqrt()) / (2.0 * a)
+        }
+    }
+
     pub fn color(&self) -> Color {
+        let t = self.hits_sphere(&V3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let n = self.at(t) - V3::new(0.0, 0.0, -1.0);
+            return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+        }
+
         let unit_dir = self.dir.unit_vector();
         let a = 0.5 * (unit_dir.y + 1.0);
 
